067e37720df3323c24de3f6fa00d69eaf079b24e
Ben Hicks <benkhicks@gmail.com>
2014-11-10 13:04:04 -0600

    Adding API wrapper around GenServer

--- lib/stack/server.ex
+++ lib/stack/server.ex
@@ -1,6 +1,31 @@
 defmodule Stack.Server do
   use GenServer
 
+  ##############
+  # External API
+  ##############
+
+  def start_link(current_stack) do
+    GenServer.start_link(__MODULE__, current_stack, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(item) do
+    GenServer.cast(__MODULE__, {:push, item})
+  end
+
+  def terminate(reason, state) do
+    IO.puts "Terminate called because reason[#{inspect reason}], current state[#{inspect state}]"
+    { :ok }
+  end
+
+  ##########################
+  # GenServer implementation
+  ##########################
+
   def handle_call(:pop, _from, current_stack) do
     [head | remaining] = current_stack
     { :reply, head, remaining }
